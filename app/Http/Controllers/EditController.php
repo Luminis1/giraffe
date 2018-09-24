cf093e50813236ed3bf7363000a9441ac0e9f8cb
Michaelle <Luminis111@gmail.com>
2018-09-24 10:36:08 +0300

    'second-fix'

--- app/Http/Controllers/EditController.php
+++ app/Http/Controllers/EditController.php
@@ -25,6 +25,10 @@ class EditController extends Controller
 
     public function saveAd(Request $request)
     {
+        $author_id = null;
+        if(!empty(Auth::user()->id)){
+            $author_id = Auth::user()->id;
+        }
         if($request['ad-id'] != null){
             $ad = Advert::find($request['ad-id']);
             $ad->title = $request['title'];
@@ -34,7 +38,7 @@ class EditController extends Controller
             Advert::create([
             'title' => $request['title'],
             'description' => $request['description'],
-            'author_id' => Auth::user()->id
+            'author_id' => $author_id
         ]);
         }
         return redirect('/');
--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -11,6 +11,11 @@ class IndexController extends Controller
 {
     public function index(Request $request)
     {
+        $author_id = null;
+        if(!empty(Auth::user()->id)){
+            $author_id = Auth::user()->id;
+        }
+
         if(empty($request['page'])) {
             $start = 0;
         }else{
@@ -24,17 +29,22 @@ class IndexController extends Controller
             'auth' => Auth::check(),
             'ads' => $ads,
             'len' => $len,
-            'user_id' => Auth::user()->id
+            'user_id' => $author_id
         ]);
     }
     public function showAd(Request $request)
     {
+        $author_id = null;
+        if(!empty(Auth::user()->id)){
+            $author_id = Auth::user()->id;
+        }
+
         $ad = Advert::find($request['id']);
 
         return view('ad',[
             'ad' => $ad,
             'auth' => Auth::check(),
-            'user_id' => Auth::user()->id
+            'user_id' => $author_id
         ]);
     }
 }
